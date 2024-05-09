b13c7c1b200d89f77c73e83072996b872a8daa36
in-git <444891953@qq.com>
2024-05-09 19:58:56 +0800

    更新文件

--- app.js
+++ app.js
@@ -10,21 +10,18 @@ folderPaths.forEach(folder => {
 
 	fs.readdir(folderPath, (err, files) => {
 		if (err) {
-			console.error(`Error reading files from ${folder}: ${err}`);
 			return;
 		}
 
 		// 构建文件路径列表
-		const filePaths = files.map(file => path.join(folder, file));
+		const filePaths = files.map(file => `${folder}/${file}`);
 
 		// 将文件路径列表写入JSON文件
 		const jsonFilePath = path.join(__dirname, `json/${folder}.json`);
 		fs.writeFile(jsonFilePath, JSON.stringify(filePaths), err => {
 			if (err) {
-				console.error(`Error writing JSON file for ${folder}: ${err}`);
 				return;
 			}
-			console.log(`JSON file created for ${folder}`);
 		});
 	});
 });
